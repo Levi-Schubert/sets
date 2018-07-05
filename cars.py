22546d4bcdc2c7d924f49dcb277c65c23e1fe635
Levi Schubert <me@levischubert.com>
2018-07-05 11:36:39 -0500

    initial commit

---
+++ cars.py
@@ -0,0 +1,6 @@
+showroom = {"Tesla Model S", "1963 Chevrolet Corvette", "Datsun 240z", "Tesla Model 3"}
+print(len(showroom))
+showroom.add("Tesla Model S")
+print(showroom)
+showroom.update({"1966 Chevrolet Nova", "Toyota Camry"})
+showroom.discard("Toyota Camry")
